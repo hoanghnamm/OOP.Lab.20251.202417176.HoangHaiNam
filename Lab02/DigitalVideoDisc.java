13871e9737e4ff60641688de47b6c17e409a98d2
unknown <nemlun0704@gmail.com>
2025-11-01 17:31:46 +0700

    topic/passing-parameter

--- Lab02/DigitalVideoDisc.java
+++ Lab02/DigitalVideoDisc.java
@@ -20,6 +20,29 @@ public class DigitalVideoDisc
         super();
         this.category=category;
     }
+
+
+public void setTitle(String title)
+{
+    this.title = title;
+}
+public void setCategory(String category)
+{
+    this.category= category;
+}
+public void setDirector(String director)
+{
+    this.director = director;
+}
+public void setLength(int length)
+{
+    this.length = length;
+
+}
+public void setCost(float cost)
+{
+    this.cost = cost;
+}
     
 public String getTitle()
 {
