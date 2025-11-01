80eb906d36921b9ae176a63233ab79f50725a661
unknown <nemlun0704@gmail.com>
2025-11-01 17:25:37 +0700

    topic/method-overloading

--- Lab02/Cart.java
+++ Lab02/Cart.java
@@ -1,52 +1,84 @@
-import java.util.LinkedList;
-public class Cart
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
     public static final int MAX_NUMBERS_ORDERED = 20;
-    LinkedList<DigitalVideoDisc> itemsOrdered =  new LinkedList<>();
+    List<DigitalVideoDisc> itemsOrdered = new ArrayList<>();
     private int qtyOrdered = 0;
-    public void addDigitalVideoDisc(DigitalVideoDisc a)
-    {
-        if(qtyOrdered < MAX_NUMBERS_ORDERED)
-        {
+
+    public void addDigitalVideoDisc(DigitalVideoDisc a) {
+        if (MAX_NUMBERS_ORDERED - qtyOrdered < 3 && MAX_NUMBERS_ORDERED-qtyOrdered>0) {
+            int b = MAX_NUMBERS_ORDERED - qtyOrdered;
+            System.out.println("The cart is almost full, there are " + b + " slots left.");
+        }
+        if (qtyOrdered >= MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is full!!!");
+        }
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered.add(a);
             qtyOrdered++;
             System.out.println("The disc has been added.");
-            System.out.println("Cost: "+ totalCost());
-        }
-        if(MAX_NUMBERS_ORDERED - qtyOrdered < 3)
-        {
-            int b = MAX_NUMBERS_ORDERED - qtyOrdered;
-            System.out.println("The cart is almost full, there are "+ b +" slots left." );
-        }
-        if(qtyOrdered >= MAX_NUMBERS_ORDERED )
-        {
-            System.out.println("The cart is full!!!");
+            System.out.println("Cost: " + totalCost());
         }
     }
-    public double totalCost()
-    {
-        double sum =0;
-        for(int i =0;i<qtyOrdered;i++)
-        {
-            sum+= itemsOrdered.get(i).getCost();
+
+    public float totalCost() {
+        float sum = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            sum += itemsOrdered.get(i).getCost();
         }
         return sum;
     }
 
-    public void removeDigitalVideoDisc(DigitalVideoDisc a)
-    {
-        if(qtyOrdered <=0)
-        {
+    public void removeDigitalVideoDisc(DigitalVideoDisc a) {
+        if (qtyOrdered <= 0) {
             System.out.println("Cannot remove.");
         }
-        if(qtyOrdered >0)
-        {
+        if (qtyOrdered > 0) {
             itemsOrdered.remove(a);
             System.out.println("Successfully removed!!!");
         }
     }
 
-    
-
-
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        if (qtyOrdered + dvdList.length < MAX_NUMBERS_ORDERED) {
+            for (int i = 0; i < dvdList.length; i++) {
+                itemsOrdered.add(dvdList[i]);
+            }
+            qtyOrdered++;
+            System.out.println( dvdList.length + " discs have been added successfully");
+            System.out.println("Total cost: " + totalCost());
+        }
+        if (qtyOrdered + dvdList.length > MAX_NUMBERS_ORDERED) {
+            if (qtyOrdered > MAX_NUMBERS_ORDERED) {
+                System.out.println("The cart is full!!!");
+            } 
+            else 
+            {
+                System.out.println("Can only add" + (MAX_NUMBERS_ORDERED-qtyOrdered) + " dvds more.");
+                for (int i = 0; i < dvdList.length; i++) {
+                    itemsOrdered.add(dvdList[i]);
+                }
+                qtyOrdered++;
+                System.out.println("The discs have been added");
+                System.out.println("Total cost: " + totalCost());
+            }
+        }
+    }
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2)
+    {
+        if(qtyOrdered >=MAX_NUMBERS_ORDERED)
+        {
+            System.out.println("The cart is full!!!");
+        }
+        if(qtyOrdered +2 <=MAX_NUMBERS_ORDERED)
+        {
+            itemsOrdered.add(dvd1);
+            itemsOrdered.add(dvd2);
+            qtyOrdered= qtyOrdered+2;
+            System.out.println("2 discs have been added successfully");
+            System.out.println("Cost: " + totalCost());
+        }
+    }
 }
--- Lab02/DigitalVideoDisc.java
+++ Lab02/DigitalVideoDisc.java
@@ -20,7 +20,28 @@ public class DigitalVideoDisc
         super();
         this.category=category;
     }
-    
+
+public void setTitle(String title)
+{
+    this.title = title;
+}
+public void setCategory(String category)
+{
+    this.category= category;
+}
+public void setDirector(String director)
+{
+    this.director = director;
+}
+public void setLength(int length)
+{
+    this.length = length;
+
+}
+public void setCost(float cost)
+{
+    this.cost = cost;
+}  
 public String getTitle()
 {
     return title;
